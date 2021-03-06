0b2e87e1940508430dee557d66d46997eee70849
Osid Abu-Alrub <abualrub.osid@gmail.com>
2021-03-06 18:31:26 +0200

    middleware to parse JSON

--- dist/main.js
+++ dist/main.js
@@ -4,5 +4,7 @@ const express = require("express");
 const config_1 = require("./config");
 // import * as op from './operations/operations';
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use("/api/category", require("./routes/api/categories"));
 app.listen(config_1.PORT, () => console.log("Server Running"));
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import * as express from "express";
 import {PORT} from './config';
 // import * as op from './operations/operations';
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 app.use("/api/category",require("./routes/api/categories"));
 
 app.listen(PORT,()=>console.log("Server Running"));
