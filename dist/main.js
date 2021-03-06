5acea21f1507cb965def08ccf13f0a57f3568519
Osid Abu-Alrub <abualrub.osid@gmail.com>
2021-03-06 18:23:58 +0200

    remove "category" from main

--- dist/main.js
+++ dist/main.js
@@ -2,9 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const config_1 = require("./config");
+// import * as op from './operations/operations';
 const app = express();
-app.get("/", (req, res) => {
-    console.log("Hello From Console");
-    res.send("Hello From Server");
-});
+app.use("/api/category", require("./routes/api/categories"));
 app.listen(config_1.PORT, () => console.log("Server Running"));
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,7 @@
 import * as express from "express";
-import {PORT,DATA} from './config';
-import * as op from './operations/operations';
+import {PORT} from './config';
+// import * as op from './operations/operations';
 const app = express();
-
-app.get("/",(req,res)=>{
-    console.log("Hello From Console");
-    res.send("Hello From Server");
-});
+app.use("/api/category",require("./routes/api/categories"));
 
 app.listen(PORT,()=>console.log("Server Running"));
